03d29cf0494d449fd7663cc31da7a0848ed014b4
Nicolas Calvi <nicolascalvi.35@gmail.com>
2022-06-02 23:52:35 +0200

    Starting a new game

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
-<script></script>
+<script>
+import Block from "./components/Block.vue";
+export default {
+  components: { Block },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      // console.log(this.delay);
+    },
+  },
+};
+</script>
 
 <template>
   <h1>Reaction Timer</h1>
+  <h2>from The Net Ninja's Tutorial</h2>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" />
 </template>
 
 <style>
@@ -9,8 +30,9 @@
 
 #app {
   max-width: 1280px;
-  margin: 0 auto;
+  margin: 60px auto;
   padding: 2rem;
   font-weight: normal;
+  text-align: center;
 }
 </style>
